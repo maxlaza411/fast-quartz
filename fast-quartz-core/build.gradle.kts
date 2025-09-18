6ea15450dc030be87144004cb753621203d8c488
Max Lazarre <53149014+maxlaza411@users.noreply.github.com>
2025-09-18 14:59:04 -0400

    Apply Fabric Loom and real Minecraft dependencies

--- fast-quartz-core/build.gradle.kts
+++ fast-quartz-core/build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    id("fabric-loom")
     `java-library`
 }
 
@@ -7,7 +8,11 @@ base {
 }
 
 dependencies {
-    api(project(":fabric-stubs"))
+    minecraft("com.mojang:minecraft:1.20.1")
+    mappings("net.fabricmc:yarn:1.20.1+build.10:v2")
+    modImplementation("net.fabricmc:fabric-loader:0.15.11")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.91.0+1.20.1")
+
     implementation("it.unimi.dsi:fastutil:8.5.13")
     implementation("org.slf4j:slf4j-api:2.0.13")
     testRuntimeOnly("org.slf4j:slf4j-simple:2.0.13")
